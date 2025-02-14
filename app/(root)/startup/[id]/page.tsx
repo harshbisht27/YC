7d76ad4298aad19681e089f08cdb3a3f9b1968a2
harshbisht27 <harshbisht180@gmail.com>
2025-02-14 11:35:09 +0530

    Some Minor Changes

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -1,55 +1,102 @@
-import SearchForm from "@/components/SearchForm";
-import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
-import { STARTUPS_QUERY } from "@/sanity/lib/queries";
-import { sanityFetch, SanityLive } from "@/sanity/lib/live";
-import { auth } from "@/auth";
+import { Suspense } from "react";
+import { client } from "@/sanity/lib/client";
+import { PLAYLIST_BY_SLUG_QUERY, STARTUP_BY_ID_QUERY } from "@/sanity/lib/queries";
+import { notFound } from "next/navigation";
+import { formatDate } from "@/lib/utils";
+import Link from "next/link";
+import Image from "next/image";
+import markdownit from "markdown-it";
+import { Skeleton } from "@/components/ui/skeleton";
+import View from "@/components/View";
+import StartupCard from "@/components/StartupCard";
 
-export default async function Home({
-  searchParams,
-}: {
-  searchParams: Promise<{ query?: string }>;
-}) {
-  const query = (await searchParams).query;
-  const params = { search: query || null };
+const md = markdownit();
 
-  const session = await auth();
+export const experimental_ppr = true;
 
+const Page = async ({ params }: { params: { id: string } }) => {
+  const id = params.id;
 
-  const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params });
+  // Fetching data from Sanity
+  const [post, editorResponse] = await Promise.all([
+    client.fetch(STARTUP_BY_ID_QUERY, { id }),
+    client.fetch(PLAYLIST_BY_SLUG_QUERY, { slug: "editor-picks-new" }),
+  ]);
+
+  if (!post) return notFound();
+
+  const editorPosts = editorResponse?.select || []; // Adjust based on the data structure
+  const parsedContent = md.render(post?.pitch || "");
 
   return (
     <>
-      <section className="pink_container">
-        <h1 className="heading">
-          Pitch Your Startup, <br />
-          Connect With Entrepreneurs
-        </h1>
-
-        <p className="sub-heading !max-w-3xl">
-          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual
-          Competitions.
-        </p>
-
-        <SearchForm query={query} />
+      <section className="pink_container !min-h-[230px]">
+        <p className="tag">{formatDate(post?._createdAt)}</p>
+        <h1 className="heading">{post.title}</h1>
+        <p className="sub-heading !max-w-5xl">{post.description}</p>
       </section>
 
       <section className="section_container">
-        <p className="text-30-semibold">
-          {query ? `Search results for "${query}"` : "All Startups"}
-        </p>
+        <img
+          src={post.image || ""}
+          alt="thumbnail"
+          className="w-full h-auto rounded-xl"
+        />
 
-        <ul className="mt-7 card_grid">
-          {posts?.length > 0 ? (
-            posts.map((post: StartupTypeCard) => (
-              <StartupCard key={post?._id} post={post} />
-            ))
+        <div className="space-y-5 mt-10 max-w-4xl mx-auto">
+          <div className="flex-between gap-5">
+            <Link
+              href={`/user/${post.author?._id}`}
+              className="flex gap-2 items-center mb-3"
+            >
+              <Image
+                src={post.author?.image || "/default-avatar.png"}
+                alt="avatar"
+                width={64}
+                height={64}
+                className="rounded-full drop-shadow-lg"
+              />
+
+              <div>
+                <p className="text-20-medium">{post.author?.name}</p>
+                <p className="text-16-medium !text-black-300">
+                  @{post.author?.username}
+                </p>
+              </div>
+            </Link>
+            <p className="category-tag">{post.category}</p>
+          </div>
+
+          <h3 className="text-30-bold">Pitch Details</h3>
+          {parsedContent ? (
+            <article
+              className="prose max-w-4xl font-work-sans break-all"
+              dangerouslySetInnerHTML={{ __html: parsedContent }}
+            />
           ) : (
-            <p className="no-results">No startups found</p>
+            <p className="no-result">No details provided</p>
           )}
-        </ul>
-      </section>
+        </div>
 
-      <SanityLive />
+        <hr className="divider" />
+
+        {editorPosts.length > 0 && (
+          <div className="max-w-4xl mx-auto">
+            <p className="text-30-semibold">Editor Picks</p>
+            <ul className="mt-7 card_grid-sm">
+              {editorPosts.map((post: any, i: number) => (
+                <StartupCard key={i} post={post} />
+              ))}
+            </ul>
+          </div>
+        )}
+
+        <Suspense fallback={<Skeleton className="view_skeleton" />}>
+          <View id={id} />
+        </Suspense>
+      </section>
     </>
   );
-}
+};
+
+export default Page;
